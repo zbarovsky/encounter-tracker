c06816049f1d3dea6470be8904973b65856af146
zbarovsky <zachary.barovsky@gmail.com>
2020-07-02 16:51:10 -0700

    changed file structure of views folder to add folders for encounter and monster

---
+++ controllers/monster.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../models');
+const axios = require('axios'); 
+// import middleware
+const flash = require('connect-flash');
+const passport = require('../config/ppConfig');
