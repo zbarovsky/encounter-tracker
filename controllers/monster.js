71e143d153c6ec913dac3f86c9e95973c62f362b
zbarovsky <zachary.barovsky@gmail.com>
2020-07-03 15:46:18 -0700

    added search bar feature that can return set data about monster searched for

--- controllers/monster.js
+++ controllers/monster.js
@@ -8,11 +8,24 @@ const passport = require('../config/ppConfig');
 //const { default: ModelManager } = require('sequelize/types/lib/model-manager');
 
 // register GET route for viewing monster page
+// router.get('/', function(req, res) {
+//     res.render('monster/view');
+// });
+
+
+// GET route for viewing monster data via API
 router.get('/', function(req, res) {
-    res.render('monster/view');
+    let dndUrl = `https://www.dnd5eapi.co/api/monsters/${req.query.index}/`
+    //console.log(dndUrl)
+    axios.get(dndUrl)
+    .then(apiResponse => {
+        let monster = apiResponse.data;
+        //console.log(monster);
+        res.render('monster/view', {monster: monster})
+    }).catch(error => {
+        console.log(error)
+    })
 });
-
-
 
 
 
